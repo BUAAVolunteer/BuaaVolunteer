2223c955fbba0e20f8fd0ec7f3e63affe7215e43
ShouHou <18373337@buaa.edu.cn>
2020-07-31 17:43:13 +0800

    confirmhistory的前端

--- miniprogram/pages/ConfirmHistory/ConfirmHistory.js
+++ miniprogram/pages/ConfirmHistory/ConfirmHistory.js
@@ -12,48 +12,52 @@ Component({
     },
   },
   data: {
-    ConfirmList: []
+    ConfirmList: [],
   },
   lifetimes: {
-    created() {
+    attached() {
       wx.showLoading({
         title: "加载中",
       });
       var that = this;
+      console.log(that.properties.title);
       // 获取目前系统时间
-      db.collection('Confirm').where({
-        title: that.properties.title
-      })
-      .get()
-      .then(res => {
-        if (res.data.length){
-          isRecord = 1;
-          ConfirmList = res.data[0].historyList;
-          for (let i = 0; i < ConfirmList.length; i++) {
-            ConfirmList[i].ID = i;
+      db.collection("confirm")
+        .where({
+          title: that.properties.title,
+        })
+        .get()
+        .then((res) => {
+          if (res.data.length) {
+            isRecord = 1;
+            let ConfirmList = res.data[0].historyList;
+            console.log(ConfirmList);
+            for (let i = 0; i < ConfirmList.length; i++) {
+              ConfirmList[i].ID = i;
+            }
+            ConfirmList = ConfirmList.reverse();
+            that.setData({
+              ConfirmList,
+            });
+          } else {
+            isRecord = 0;
+            wx.showModal({
+              title: "错误",
+              content: "没有该志愿的确认记录，请先导出报名表",
+              showCancel: false,
+            });
           }
-          that.setData({
-            ConfirmList,
-          })
-        }else{
-          isRecord = 0;
+          wx.hideLoading();
+        })
+        .catch((err) => {
+          console.log(err);
+          wx.hideLoading();
           wx.showModal({
             title: "错误",
-            content: "没有该志愿的确认记录，请先导出报名表",
+            content: "没有找到记录，请检查网络或重启小程序",
             showCancel: false,
           });
-        }
-        wx.hideLoading();
-      })
-      .catch(err =>{
-        console.log(err)
-        wx.hideLoading();
-        wx.showModal({
-          title: "错误",
-          content: "没有找到记录，请检查网络或重启小程序",
-          showCancel: false,
         });
-      })
     },
   },
   /**
@@ -65,8 +69,8 @@ Component({
       // console.log(e.target.id);
       let ID = e.detail.ID;
       var confirmList_json = this.data.ConfirmList[ID];
-      wx.navigateTo({ 
-        url: 'Confirm/Confirm?confirmList=' + JSON.stringify(confirmList_json)
+      wx.navigateTo({
+        url: "Confirm/Confirm?confirmList=" + JSON.stringify(confirmList_json),
       });
     },
   },
