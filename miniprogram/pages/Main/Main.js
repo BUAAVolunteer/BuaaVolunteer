422b092b39606c7296eea20ebbb9586f17356c5d
ShouHou <18373337@buaa.edu.cn>
2020-08-09 11:52:04 +0800

    history

--- miniprogram/pages/Main/Main.js
+++ miniprogram/pages/Main/Main.js
@@ -12,7 +12,7 @@ Component({
     mainIcon: [
       {
         navigateUrl: "/pages/Main/Activity/Activity", // 要跳转到的页面路径
-        iconSrc: "/assests/icons/1.png", // 按钮图标的路径
+        iconSrc: "/assets/icons/1.png", // 按钮图标的路径
         iconText: "近期活动", // 按钮的文本
         width: "724.64rpx", // 按钮的宽度
         backgroundColor: "#22a2c3", // 按钮的背景颜色
@@ -20,7 +20,7 @@ Component({
       },
       {
         navigateUrl: "/pages/Introduction/Introduction",
-        iconSrc: "/assests/icons/2.png",
+        iconSrc: "/assets/icons/2.png",
         iconText: "志愿项目",
         width: "656.64rpx",
         backgroundColor: "#5fc1c7",
@@ -28,7 +28,7 @@ Component({
       },
       {
         navigateUrl: "/pages/Main/FeedBack/FeedBack",
-        iconSrc: "/assests/icons/3.png",
+        iconSrc: "/assets/icons/3.png",
         iconText: "问题反馈",
         width: "588.64rpx",
         backgroundColor: "#f9d770",
@@ -37,7 +37,7 @@ Component({
       {
         navigateUrl:
           "/pages/OuterLink/OuterLink?url=https://mp.weixin.qq.com/s/5bqJNvDXhH8j9iGZ5diyMw",
-        iconSrc: "/assests/icons/4.png",
+        iconSrc: "/assets/icons/4.png",
         iconText: "蓝协介绍",
         width: "520.64rpx",
         backgroundColor: "#f68c60",
@@ -46,7 +46,7 @@ Component({
       {
         navigateUrl:
           "/pages/OuterLink/OuterLink?url=https://mp.weixin.qq.com/s/cgU6BbeFxHXXsWwl5wePTw",
-        iconSrc: "/assests/icons/5.png",
+        iconSrc: "/assets/icons/5.png",
         iconText: "联系我们",
         width: "452.64rpx",
         backgroundColor: "#f07c82",
---
+++ miniprogram/pages/Profile/childCpn/history-card/history-card.js
@@ -0,0 +1,32 @@
+// pages/Profile/childCpn/history-card/history-card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    history: {
+      type: Object,
+      value: {
+        note: "", //志愿备注
+        title: "", // 志愿名称
+        score: ",", // 志愿得分
+        duration: 0, // 志愿时长
+      },
+    },
+  },
+  lifetimes:{
+    created() {
+      console.log(this.properties.history)
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+});
