e0fbdaf0f37c232caa0a8ebe1a40af93f50ea253
WingDr <krq2000@126.com>
2020-08-22 23:13:07 +0800

    完善详细信息bug，详细信息以及表单编辑流程测试完成

--- miniprogram/pages/Manage/Detail/Detail.js
+++ miniprogram/pages/Manage/Detail/Detail.js
@@ -47,55 +47,17 @@ Component({
             if (res.data.length == 0) {
               wx.hideLoading();
               return
-            } else if (res.data[0].assure instanceof Array) {
-              let download = res.data[0];
-              var assure = download.assure
-              .reduce(function (preValue, n) {
-                return preValue + n + "\n";
-              }, "")
-              .slice(0, -1);
-
-              var detail = download.detail
-                .reduce(function (preValue, n) {
-                  return preValue + n + "\n";
-                }, "")
-                .slice(0, -1);
-
-              var require = download.require
-                .reduce(function (preValue, n) {
-                  return preValue + n + "\n";
-                }, "")
-                .slice(0, -1);
-
-              var response = download.response
-                .reduce(function (preValue, n) {
-                  return preValue + n + "\n";
-                }, "")
-                .slice(0, -1);
-
-              var people = download.people
-                .reduce(function (preValue, n) {
-                  return preValue + n + "\n";
-                }, "")
-                .slice(0, -1);
-            } else {
-              let download = res.data[0];
-              var assure = download.assure
-              var detail = download.detail
-              var require = download.require
-              var response = download.response
-              var people = download.people
             }
-
+            var download = res.data[0];
             that.setData({
               time: download.time, //一坨数据存在云端
               date: download.date,
               textarea: download.textarea,
-              assure, //拼接的用于屏幕输出的字符串
-              detail, //同名就不用再写一遍赋值
-              require,
-              response,
-              people,
+              assure: download.assure, //拼接的用于屏幕输出的字符串
+              detail: download.detail, //同名就不用再写一遍赋值
+              require: download.require,
+              response: download.response,
+              people: download.people,
               place: download.place,
               qqNum: download.qqNum,
             });
@@ -106,7 +68,7 @@ Component({
   },
   methods: {
     illegalCheck (that) {
-      console.log(that)
+      //console.log(that)
       if (that.data.title === "发布一个新志愿") {
         wx.showModal({
           title: "缺少信息",
@@ -140,25 +102,37 @@ Component({
         });
         return false;
       }
-      wx.cloud.callFunction({
-        name: "getTime",
-        success: function (res) {
-          //console.log(res)
-          //返回值是日期和时间
-          var time = res.result.time.split(" ");
-          var currentTime = time[1];
-          var currentDate = time[0];
-          if (
-            currentDate > that.data.date ||
-            (currentDate == that.data.date && currentTime > that.data.time)
-          ) {
-            wx.showModal({
-              title: "信息错误",
-              content: "志愿发布时间不能在当前时间之前",
-              showCancel: false,
-            });
-            return false;
-          }
+
+      console.log(that.data.date)
+      return new Promise ((resolve, reject) => {
+        resolve()
+      })
+      .then(() => {
+        return wx.cloud.callFunction({
+          name: "getTime",
+        })
+      })
+      .then(res => {
+        console.log(res)
+        //返回值是日期和时间
+        var time = res.result.time.split(" ");
+        var currentTime = time[1];
+        var currentDate = time[0];
+        if (
+          currentDate > that.data.date ||
+          (currentDate === that.data.date && currentTime > that.data.time)
+        ) {
+          console.log(0)
+          wx.showModal({
+            title: "信息错误",
+            content: "志愿发布时间不能在当前时间之前",
+            showCancel: false,
+          });
+          return false
+        } else {
+          console.log(currentDate)
+          console.log(that.data.date)
+          return true
         }
       })
     },
@@ -171,56 +145,59 @@ Component({
       //合法性校验
       console.log(that)
       console.log(that.__proto__)
-      var isPass = that.__proto__.illegalCheck(that);
-      if (!isPass) {
-        wx.hideLoading();
-        return
-      }
-      //上传详细信息
-      wx.cloud.callFunction({
-        //调用这个云函数
-        name: "uploadvolun",
-        data: {
-          originTitle: that.data.originTitle,
-          title: that.data.title,
-          date: that.data.date,
-          time: that.data.time,
-          textarea: that.data.textarea,
-          place: that.data.place,
-          people: that.data.people,
-          assure: that.data.assure,
-          detail: that.data.detail,
-          require: that.data.require,
-          response: that.data.response,
-          innerList: [],
-          signupList: [],
-          qqNum: that.data.qqNum,
-        },
-        success: function (e) {
-          console.log(e);
+      that.illegalCheck(that).then(res => {
+        console.log(res)
+        if (!res) {
           wx.hideLoading();
-          wx.showModal({
-            title: "发布成功",
-            content: "志愿发布成功，请编辑报名表单",
-            showCancel: false, //去掉取消按钮
-            success: function (res) {
-              //如果成功调用showModal成功，则跳转至链接
-              wx.redirectTo({
-                url: "../Manage",
+          return
+        } else {
+          //上传详细信息
+          wx.cloud.callFunction({
+            //调用这个云函数
+            name: "uploadvolun",
+            data: {
+              originTitle: that.data.originTitle,
+              title: that.data.title,
+              date: that.data.date,
+              time: that.data.time,
+              textarea: that.data.textarea,
+              place: that.data.place,
+              people: that.data.people,
+              assure: that.data.assure,
+              detail: that.data.detail,
+              require: that.data.require,
+              response: that.data.response,
+              innerList: [],
+              signupList: [],
+              qqNum: that.data.qqNum,
+            },
+            success: function (e) {
+              console.log(e);
+              wx.hideLoading();
+              wx.showModal({
+                title: "发布成功",
+                content: "志愿发布成功，请编辑报名表单",
+                showCancel: false, //去掉取消按钮
+                success: function (res) {
+                  //如果成功调用showModal成功，则跳转至链接
+                  wx.redirectTo({
+                    url: "../Manage",
+                  });
+                },
+              });
+            },
+            fail: function (e) {
+              wx.hideLoading();
+              console.log(e);
+              wx.showModal({
+                title: "发布失败",
+                content: "有错误发生，发布失败",
+                showCancel: false,
               });
             },
           });
-        },
-        fail: function (e) {
-          wx.hideLoading();
-          console.log(e);
-          wx.showModal({
-            title: "发布失败",
-            content: "有错误发生，发布失败",
-            showCancel: false,
-          });
-        },
-      });
+        }
+      })
     },
   },
 });
