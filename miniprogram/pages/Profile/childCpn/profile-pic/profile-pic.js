06c0481fd57d41746de3e2c7871eb3f4e4c4a699
ShouHou <18373337@buaa.edu.cn>
2020-08-27 14:32:56 +0800

    修改头像

--- miniprogram/pages/Profile/childCpn/profile-pic/profile-pic.js
+++ miniprogram/pages/Profile/childCpn/profile-pic/profile-pic.js
@@ -1,23 +1,64 @@
 // pages/Profile/ProfilePic/ProfilePic.js
+const app = getApp();
+const db = wx.cloud.database();
+const _ = db.command;
 Component({
   /**
    * 组件的属性列表
    */
-  properties: {
-
-  },
+  properties: {},
 
   /**
    * 组件的初始数据
    */
   data: {
-    
+    headList: [], // 头像列表
+    curAvator: app.globalData.avator,
   },
-
+  /**
+   * 组件的生命周期
+   */
+  created() {
+    // head获取所有头像信息
+    db.collection("head")
+      .get()
+      .then((res) => {
+        let qualification = app.globalData.qualification;
+        for (let i = 0; i < res.data.length; i++) {
+          if (res.data[i].qualification == "") {
+            res.data[i].isShow = true;
+          } else {
+            let flag = 1;
+            for (let j = 0; j < qualification.length; j++) {
+              if (qualification[j] == res.data[i].qualification) {
+                res.data[i].isShow = true;
+                flag = 0;
+                break;
+              }
+            }
+            if (flag) {
+              res.data[i].isShow = false;
+            }
+          }
+        }
+        console.log(res.data);
+        this.setData({
+          headList: res.data,
+        });
+      });
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-    
-  }
-})
+    // 选择头像
+    choosePic(e) {
+      console.log(e);
+      app.globalData.avator = e.currentTarget.id;
+      this.setData({
+        curAvator: e.currentTarget.id,
+      });
+      // 缺一个，上传头像的方法
+    },
+  },
+});
