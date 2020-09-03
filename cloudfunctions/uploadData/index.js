3afe2d670cba8fadd452f6be8995c943e8480f17
WingDr <krq2000@126.com>
2020-09-03 13:19:39 +0800

    修复uploadData云函数

--- cloudfunctions/uploadData/index.js
+++ cloudfunctions/uploadData/index.js
@@ -13,36 +13,34 @@ const _ = db.command;
 exports.main = async (event, context) => {
 	try {
 		let uploadList = event.list;
+		let success
+		console.log(event)
 		return promise = new Promise(function (resolve,reject) {
 			resolve();
 		})
 		.then(() => {
-			return signUpCheck(0,event);
+			return signUpCheck(event);
 		})
 		.then(res => {
 			console.log(res)
-			return new Promise((resolve,reject) =>{
-				let dtu = dataUpload(res.stopPlace - 1, event, res.success);
-				if (!res.success & dtu) {
-					reject("error");
-				}else{
-					resolve();
-				}
-			})
+			return dataUpload(event, res)
 		})
-		.then(() => {
+		.then(res => {
 			//console.log(uploadList)
 			//记录已经报名成功的人的openid
-			var l = uploadList[0].length - 3
-			var openid = uploadList[0][l]
-			console.log(openid)
-			return db.collection('project').where({
-				title: event.title
-			}).update({
-				data:{
-					signupList: _.push(openid)
-				}
-			})
+			success = res
+			if (res) {
+				var l = uploadList[0].length - 3
+				var openid = uploadList[0][l]
+				console.log(openid)
+				return db.collection('project').where({
+					title: event.title
+				}).update({
+					data:{
+						signupList: _.push(openid)
+					}
+				})
+			}
 		})
 		.then(() =>{
 			//获得表单数据
@@ -55,9 +53,9 @@ exports.main = async (event, context) => {
 			//判断是否报名完毕
 			let over = 1;
 			for (let i = 0; i < res.data[0].formInfo.length; i++){
-				if (res.data[0].formInfo[i].limit){
-					for (let j = 0; j < res.data[0].formInfo[i].data.length; j++){
-						if (res.data[0].formInfo[i].data[j].limit > 0){
+				if (res.data[0].formInfo[i].isLimit){
+					for (let j = 0; j < res.data[0].formInfo[i].option.length; j++){
+						if (res.data[0].formInfo[i].option[j].limit > 0){
 							over = 0;
 							break;
 						}
@@ -88,36 +86,19 @@ exports.main = async (event, context) => {
 			}
 		})
 		.then(() => {
-			//向个人数据中添加报名成功记录
-			let inf = {};
-			inf.duration = "待定";
-			inf.title = event.title;
-			inf.note = "待定";
-			inf.score = "待定";
-			inf.signUpTime = event.signUpTime;
-			console.log(inf)
-			let phone = uploadList[0][1];
-			db.collection('person').where({
-				phone: phone
-			}).update({
-				data:{
-					history: _.push(inf)
-				}
-			})
+			console.log(success)
+			if (success) {
+				return afterSuccess(event)
+			} else {
+				return false
+			}
 		})
 		.then(() => {
-			//上传报名数据
-			db.collection('signUp').where({
-				title: event.title
-			}).update({
-				// data 传入需要局部更新的数据
-				data: {
-					list : _.push(uploadList)
-				}
-			})
-		})
-		.then(() => {
-			return 'SignUpSuccess'
+			if (success) {
+				return 'SignUpSuccess'
+			} else {
+				return 'error'
+			}
 		})
 		.catch(err => {
 			console.log(err)
@@ -128,82 +109,107 @@ exports.main = async (event, context) => {
 	}
 }
 
-function signUpCheck(position,event) {
+function signUpCheck(event) {
 	//名额增减的递归函数
-	let i = position;
-	let upPosition = "formInfo." + event.limit[0][i] + ".option." + event.limit[1][i] + ".bookingNum";
-	return db.collection('form').where({
-		title: event.title,
-	}).update({
-		data:{
-			[upPosition] : _.inc(1)
-		}
-	}).then(() => {
-		return db.collection('form').where({
-			title: event.title
-		}).field({
-			formInfo: true
-		}).get()
-	}).then(res => {
-		let temp = res.data[0].formInfo[event.limit[0][i]].option[event.limit[1][i]].bookingNum;
-		let limit = res.data[0].formInfo[event.limit[0][i]].option[event.limit[1][i]].limit;
-		i++;
-		if (temp > limit){
-			//无名额，返回报名失败
-			console.log("fail")
-			let checkResult = {}
-			checkResult.success = false;
-			checkResult.stopPlace = i;
-			return checkResult;
-		}
-	}).then(() => {
-		if (i < event.limit[0].length) {
-			console.log("continue")
-			signUpCheck(i,event);
-		} else {
-			console.log("success")
-			let checkResult = {}
-			checkResult.success = true;
-			checkResult.stopPlace = i;
-			return checkResult;
-		}
+	var promiseList = []
+	var success = true
+	for (let i = 0; i < event.limit[0].length; i++) {
+		var promise = new Promise((resolve, reject) => {
+			let upPosition = "formInfo." + event.limit[0][i] + ".option." + event.limit[1][i] + ".bookingNum";
+			resolve(
+				db.collection('form').where({
+					title: event.title,
+				}).update({
+					data:{
+						[upPosition] : _.inc(1)
+					}
+				}).then(() => {
+					return db.collection('form').where({
+						title: event.title
+					}).field({
+						formInfo: true
+					}).get()
+				}).then(res => {
+					let temp = res.data[0].formInfo[event.limit[0][i]].option[event.limit[1][i]].bookingNum;
+					let limit = res.data[0].formInfo[event.limit[0][i]].option[event.limit[1][i]].limit;
+					i++;
+					if (temp > limit){
+						//无名额，返回报名失败
+						console.log("fail")
+						success = false
+					}
+				})
+			)
+		})
+		promiseList.push(promise)
+	}
+	return Promise.all(promiseList).then(() => {
+		return success
 	})
 }
 
-function dataUpload(position,event,success) {
-	let i = position;
-	let upPosition = "formInfo." + event.limit[0][i] + ".option." + event.limit[1][i] + ".bookingNum";
-	let limPosition = "formInfo." + event.limit[0][i] + ".option." + event.limit[1][i] + ".limit"
-	if (!success){
-		return db.collection('form').where({
-			title: event.title
-		}).update({
-			data:{
-				[upPosition]: _.inc(-1)
-			}
-		}).then(() => {
-			i--;
-			if (i >= 0) {
-				dataUpload(i,event,success);
-			} else {
-				return true;
-			}
-		})
-	}else{
-		return db.collection('form').where({
-			title: event.title
-		}).update({
-			data:{
-				[limPosition]: _.inc(-1),
-				[upPosition]: _.inc(-1)
-			}
-		}).then(() => {
-			i--;
-			if (i >= 0) {
-				dataUpload(i,event,success);
-			} else {
-				return true;
+function dataUpload(event,success) {
+	var promiseList = []
+	for (let i = 0; i < event.limit[0].length; i++) {
+		var promise = new Promise((resolve,reject) => {
+			let upPosition = "formInfo." + event.limit[0][i] + ".option." + event.limit[1][i] + ".bookingNum";
+			let limPosition = "formInfo." + event.limit[0][i] + ".option." + event.limit[1][i] + ".limit"
+			if (!success){
+				resolve(
+					db.collection('form').where({
+						title: event.title
+					}).update({
+						data:{
+							[upPosition]: _.inc(-1)
+						}
+					})
+				)
+			}else{
+				resolve(
+					db.collection('form').where({
+						title: event.title
+					}).update({
+						data:{
+							[limPosition]: _.inc(-1),
+							[upPosition]: _.inc(-1)
+						}
+					})
+				)
 			}
 		})
+		promiseList.push(promise)
 	}
+	return Promise.all(promiseList).then(() => {
+		return success
+	})
+}
+
+function afterSuccess(event) {
+	//向个人数据中添加报名成功记录
+	let inf = {};
+	inf.duration = "待定";
+	inf.title = event.title;
+	inf.note = "待定";
+	inf.score = "待定";
+	inf.signUpTime = event.signUpTime;
+	console.log(inf)
+	let phone = event.list[0][1];
+	return db.collection('person').where({
+		phone: phone
+	}).update({
+		data:{
+			history: _.push(inf)
+		}
+	})
+	.then(() => {
+		//上传报名数据
+		return db.collection('signUp').where({
+			title: event.title
+		}).update({
+			// data 传入需要局部更新的数据
+			data: {
+				list : _.push(event.list)
+			}
+		})
+	})
 }
