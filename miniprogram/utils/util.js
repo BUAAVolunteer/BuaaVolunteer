ca0e9f92115403c03c3bbe1d1afb96d24e04f968
wingdr <krq2000@126.com>
2021-03-17 18:55:30 +0800

    修复冲突

--- miniprogram/utils/util.js
+++ miniprogram/utils/util.js
@@ -40,29 +40,6 @@ class Util {
         return n[1] ? n : '0' + n;
     };
 
-<<<<<<< HEAD
-    static exportToExcel (exportExcelInfo) {
-        //检查目录是否存在
-        var cachePath = wx.env.USER_DATA_PATH + '/cache';
-        var fm = wx.getFileSystemManager();
-        return new Promise(function (resolve, reject) {
-            fm.access({
-                path: cachePath,
-                success: function (res) {
-                    resolve(res)
-                },
-                fail: function (err) {
-                    //不存在则创建目录
-                    fm.mkdir({
-                        dirPath: cachePath,
-                        recursive: true,
-                        success: function (res) {
-                            resolve(res);
-                        },
-                        fail: function (err) {
-                            reject({
-                                checkPath: 1
-=======
     static exportToExcel (exportInfo) {
             //检查目录是否存在
         var cachePath = wx.env.USER_DATA_PATH + '/cache';
@@ -84,13 +61,11 @@ class Util {
                         fail:function(err){
                             reject({
                                 checkPath:1
->>>>>>> Xin
                             })
                         }
                     })
                 }
             })
-<<<<<<< HEAD
         }).then(res => {
             return wx.cloud.callFunction({
                 name: 'exportData',
@@ -100,118 +75,11 @@ class Util {
                     exportList: exportExcelInfo.downloadList
                 }
 
-=======
-        }).then(res=>{
-            return wx.cloud.callFunction({
-                name: 'exportData',
-                data: {
-                    title : exportInfo.title,
-                    fileName : exportInfo.fileName,
-                    exportList : exportInfo.downloadList
-                }
-               
->>>>>>> Xin
             })
         }).then(res => {
             console.log(res)
             console.log("到这里")
             //下载Excel
-<<<<<<< HEAD
-            return new Promise(function (resolve, reject) {
-                var dlF = wx.cloud.downloadFile;
-                //  var sF = wx.saveFile;
-                dlF({
-                    fileID: res.result.fileID,
-                    success: function (res) {
-                        console.log("halohalohalo")
-                        fm.saveFile({
-                            tempFilePath: res.tempFilePath,
-                            FilePath: cachePath + '/' + exportExcelInfo.fileName + ".xlsx",
-                            success: function (res) {
-                                resolve(res);
-                            },
-                            fail: function (err) {
-                                reject(err);
-                            }
-                        })
-                    },
-                    fail: function (err) {
-                        reject(err);
-                    }
-                })
-            })
-        })
-
-
-
-            .then(res => {
-                //console.log(res.savedFilePath)
-                console.log("finally")
-                //自动打开Excel
-                return new Promise(function (resolve, reject) {
-                    var oD = wx.openDocument
-                    oD({
-                        filePath: res.savedFilePath,
-                        fileType: "xlsx",
-                        showMenu: true,
-                        success: function (res) {
-                            resolve(res);
-                        },
-                        fail: function (err) {
-                            reject(err);
-                        }
-                    })
-                })
-            }).then(res => {
-                let returnData = {};
-                returnData.success = true;
-                returnData.res = res;
-                return returnData;
-            }).catch(err => {
-                let returnData = {};
-                returnData.success = false;
-                returnData.res = err;
-                return returnData;
-            })
-    };
-
-    static exportToImage (exportImgInfo) {
-        //检查目录是否存在
-        var cachePath = wx.env.USER_DATA_PATH + '/cache';
-        var im = wx.getFileSystemManager();
-        return new Promise(function (resolve, reject) {
-            im.access({
-                path: cachePath,
-                success: function (res) {
-                    resolve(res)
-                },
-                fail: function (err) {
-                    //不存在则创建目录
-                    fm.mkdir({
-                        dirPath: cachePath,
-                        recursive: true,
-                        success: function (res) {
-                            resolve(res);
-                        },
-                        fail: function (err) {
-                            reject({
-                                checkPath: 1
-                            })
-                        }
-                    })
-                }
-            })
-        }).then(res => {
-            return wx.cloud.callFunction({
-                name: 'exportData',
-                data: {
-                    title: exportImgInfo.title,
-                    fileName: exportImgInfo.fileName,
-                    exportList: exportImgInfo.downloadList
-                }
-
-            })
-=======
             return new Promise(function(resolve, reject){
                 var dlF = wx.cloud.downloadFile;
               //  var sF = wx.saveFile;
@@ -257,7 +125,6 @@ class Util {
                     }
                 })
             }) 
->>>>>>> Xin
         }).then(res => {
             console.log(res)
             console.log("到这里")
